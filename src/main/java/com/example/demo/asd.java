8608c8eba5562df5d28d0bda5bfd414d38dad114
msq <1528942313@qq.com>
2019-11-12 09:33:37 +0800

    第一个版本

---
+++ src/main/java/com/example/demo/asd.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+/**
+ * @author ：msq
+ * @类名 ：这是asd类
+ * @date ：2019/11/12   9:32
+ * @description ：df
+ */
+public class asd {
+    public static void main(String[] args) {
+
+    }
+}
